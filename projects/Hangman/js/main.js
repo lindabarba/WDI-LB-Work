9cb650e585a39c3ea9c7da6136a47f70721c6d3d
Linda Barba <lindabarba13@gmail.com>
2017-04-03 13:45:23 -0700

    chnage some html classes add js for init funcs click listeners

--- projects/Hangman/js/main.js
+++ projects/Hangman/js/main.js
@@ -1,35 +1,76 @@
 /*--- Hangman - Project 1 - WDI-DT-44-LB ---*/
 
 /*--- variables ---*/
-var player1Word = []; // input/word or phrase submitted by Player 1
-var letterEntered = []; // input/letters, word, or phrase submitted by Player 2
-var player2Word = []; // cummulatively holds value of 'correct' lettersEntered
-var badLetters = []; // cummulatively holds value of 'incorrect' lettersEntered
+var secretWord;
+var guessWord;
+var badLetters;
+var message;
+
 var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var gallows = []; //not sure if this is necessary; to hold value of divs to flip
+
+// cache dom elements
+var messageEl = document.getElementById('message');
 
 /*--- event listeners ---*/
-document.querySelector('button').addEventListener('click', newGame);
-document.getElementsByClassName('keyboard-cell').addEventListener('click', choice);
+document.querySelector('button').addEventListener('click', startNewGame);
+document.getElementById('keyboard-table').addEventListener('click', handleLetterChoice);
 
 /*--- functions ---*/
-function start() {
-  var message = document.getElementById('message');
-  message.textContent = 'Hey, want to play Hangman?'
+function initialize() {
+  message = 'Hey, want to play Hangman?';
+  secretWord = [];
+  guessWord = [];
+  badLetters = [];
+  render();
 }
 
 /*this will reset board/start new game*/
-function newGame() {
-  start();
-  console.log("hello");
+function startNewGame() {
+  message = 'Awesome!'
+  // get player2 to pick phrase...
+  getSecretWord();
+  render();
 }
 
-function choice(event) {
+function getSecretWord() {
+  message = 'Using the keyboard below, enter a word for your opponent to guess.'
+  //use keyboard, have each letter be added to secretWord
+
+}
+
+function handleLetterChoice(event) {
   var letter = event.target.textContent;
   //
-  console.log("goodbye");
+  console.log(letter);
+
+
+  render();
 }
+
+function render() {
+  messageEl.textContent = message;
+  renderSecretWord ();
+  //renderGuessWord ();
+  renderLetterChoice();
+}
+
+function renderSecretWord() {
+  /* add children to DOM element word-row by using length of secretWord
+  to add that many tds w/ class word-cell
+  add textContent to each child
+  use innerHTML to hide this */
+  //function renderLetterChoice();
+}
+
+function renderLetterChoice() {
+/*  alphabet.forEach(function(letter) {
+    // select the letter dom
+    var letterEl = document.getElementById(letter);
+    var class = (badLetters.includes(letter) || guessWord.includes(letter)) ? 'disable-letter' : '';
+    letterEl.addClass(class);
+  });
+*/}
 
 /*
 function click(e) {
@@ -52,10 +93,10 @@ function click(e) {
 
 /*pseudocode
 Start with blank slate to allow for 7 guesses (noose, head, body, 2 arms, 2 legs):
-1. Initialize state (blank canvas).
+DONE: 1. Initialize state (blank canvas).
 2. Render board - gallows
 3. Define variables
-    a. player1Word - Word submitted
+    a. secretWord - Word submitted
     b. Players
         -player1 - enters the word or phrase to be guessed by Player 2
         -player2 - guesses the word or phrase entered by Player 1
@@ -65,23 +106,23 @@ Start with blank slate to allow for 7 guesses (noose, head, body, 2 arms, 2 legs
     b. Handling entire phrase submit (win/lose?).
 5. letterEntered is 'read'
     a. must be in alphabet
-    b. check against player1Word
-        i. If letterEntered is in player1Word then append player2Word
+    b. check against secretWord
+        i. If letterEntered is in secretWord then append guessWord
           corresponding to correct index (ie if letter 'a' is at
-          index 0 of player1Word and letterEntered is 'a', letterEntered
-           must be dropped into player2Word at index 0)
-        ii. If letterEntered is not in player1Word then:
+          index 0 of secretWord and letterEntered is 'a', letterEntered
+           must be dropped into guessWord at index 0)
+        ii. If letterEntered is not in secretWord then:
             1. use innerHTML to reveal different gallows div
             2. drop letterEntered into player2Wrong, no specific order
             ** these two functions do not need to be asynchronous
 8. Remove letterEntered from alphabet
 9. Determine win or death (state of game).
-    a. player1Word === player2Word = win
+    a. secretWord === guessWord = win
     b. gallows === full = loss
     c. render
     d. else next letterEntered
 10. Handling reset game.
 */
-//start();
+initialize();
 
 
